135683028b749c8272af272e2ebaa841f393023f
rendikaaa <114813064+rendikaaa@users.noreply.github.com>
2023-10-20 21:55:00 +0700

    Add files via upload

---
+++ sesi3-1.js
@@ -0,0 +1,15 @@
+const restaurant = [
+    {
+      name: "Rumah Mesra",
+      city: "Sukabumi",
+      favoriteDrink: "Ice Coffee",
+      favoriteFood: "Dimsum",
+      isVegan: true,
+    },
+  ];
+  
+  console.log("Nama : " + restaurant[0].name);
+  console.log("Kota : " + restaurant[0].city);
+  console.log("Minuman Favorit : " + restaurant[0].favoriteDrink);
+  console.log("Makanan Favorit : " + restaurant[0].favoriteFood);
+  console.log("Vegan : " + restaurant[0].isVegan);
---
+++ sesi3-2.js
@@ -0,0 +1,14 @@
+const restaurant = {
+    name: "Restoe Ibu",
+    city: "Sukabumi",
+    favoriteDrink: "Es Teh Manis",
+    favoriteFood: "Mie Nyemeuk",
+    isVegan: false,
+  };
+  
+  const { name, city, favoriteDrink, favoriteFood, isVegan } = restaurant;
+  console.log("Nama : " + name);
+  console.log("Kota : " + city);
+  console.log("Minuman Favorit : " + favoriteDrink);
+  console.log("Makanan Favorit : " + favoriteFood);
+  console.log("Vegan : " + isVegan);
